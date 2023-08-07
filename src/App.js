007208253ffe25bc513960b664ce03bdbffd7e34
nguyenson2012 <traibachkhoa2012@gmail.com>
2023-08-08 06:35:31 +0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+export default App;
