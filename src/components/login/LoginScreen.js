786af4ecdaf76fbf1cca50cbf907318ecee13b06
nguyenson2012 <traibachkhoa2012@gmail.com>
2023-08-17 08:06:24 +0700

    Add image resource

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -20,7 +20,7 @@ const LoginScreen = () => {
           <div style={{ padding: "20px" }}>
             <Header />
             <GridLayout
-              image={<Image src="/images/avatars/" preview={false} />}
+              image={<Image src="/images/avatars/marvel.jpeg" preview={false} />}
               form={<LoginForm onLoading={handleLoading} />}
               loading={loading}
             />
